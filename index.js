5477e457bcd29b0f01fff2e01017fde2834902f0
gayathri-eigertech <gyedupati@lainternal.com>
2020-07-21 16:16:54 +0530

    final commit

--- index.js
+++ index.js
@@ -2,70 +2,90 @@ const fetch = require('node-fetch')
 const axios = require('axios');
 
 let url = "http://norvig.com/big.txt";
-
+/** fetches the document from a given url */
 fetch(url)
     .then(response => response.text())
     .then(data => {
         var wordsArr = splitWords(data)
         var words = wordsCount(wordsArr)
         var count = sortByCount(words)
-        
-        //top 10 words
-         jsonObj =[]
-        for(var i=0;i<10;i++){
-            axios({
-                method: 'post',
-                url: 'https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=dict.1.1.20170610T055246Z.0f11bdc42e7b693a.eefbde961e10106a4efa7d852287caa49ecc68cf&lang=en-en&text='+`${count[i].name}`,
-              })
-                .then(function (response) {
-                 jsonObj.push({"word":`${count[i].name}`})
-                 item ={}
-                 item["count"] = `${count[i].total}`
-                 if(response.data.def[0].hasOwnProperty(pos)){
-                 item["Pos"] =response.data.def[0].pos
-                }else{
-                    item["Pos"] =""
+        jsonArr = []
+        for (var i = 0; i < count.length; i++) {
+            item = {}
+            item["Count"] = count[i].total
+            item["Word"] = count[i].name
+            jsonArr.push(item)
+            dictCall(count[i].name).then(result => {
+                let dictRes = result.data.def[0]
+                if (dictRes != null) {
+                    JsonOutput = []
+                    for (var j = 0; j < jsonArr.length; j++) {
+                        if (jsonArr[j].Word === result.data.def[0].text) {
+                            // console.log(result.data.def[0].pos)
+                            items = {}
+                            items["Count"] = jsonArr[j].Count
+                            items["Word"] = jsonArr[j].Word
+                            items["Pos"] = result.data.def[0].pos
+
+                            JsonOutput.push(items)
+
+                        }
+                    }
+                   
+                    console.log(JSON.stringify(JsonOutput))
                 }
-                 jsonObj.push(item)
-                 //item[""] =response.data.def[0].pos
-                 //console.log(response.data.def[0].pos)
-                });  
+
+            })
 
         }
-       console.log(JSON.stringify(jsonObj))
     });
 
-
+/**function will split the words in doc by empty space */
 function splitWords(text) {
     var wordsArr = text.split(/\s+/)
     return wordsArr;
 }
 
+/** function counts the repeated words in a words array. */
 function wordsCount(wordsArr) {
     var words = {}
 
-    wordsArr.forEach(function (key) {
-        
-            if (words.hasOwnProperty(key)) {
-                words[key]++;
-            } else {
-                words[key] = 1
-            }
-        
+    wordsArr.forEach(key => {
+
+        if (words.hasOwnProperty(key)) {
+            words[key]++;
+        } else {
+            words[key] = 1
+        }
+
     });
     return words;
 }
-
+/** underlying function will sorts word count in descending order and returns top 10 words by occurance. */
 function sortByCount(words) {
     var topTenWords = [];
-    topTenWords = Object.keys(words).map(function (key) {
+    topTenWords = Object.keys(words).map(key => {
         return {
             name: key,
             total: words[key]
         };
     });
-    topTenWords.sort(function (a, b) {
-        return b.total -a.total;
+    topTenWords.sort((a, b)=>{
+        return b.total - a.total;
     });
-    return topTenWords;
+    return topTenWords.slice(0, 12);
+}
+/** underlying function will will call the dictionary api and returns the response. */
+async function dictCall(word) {
+    //top 10 words
+    try {
+        const resp = await axios({
+            method: 'post',
+            url: 'https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=dict.1.1.20170610T055246Z.0f11bdc42e7b693a.eefbde961e10106a4efa7d852287caa49ecc68cf&lang=en-en&text=' + `${word}`
+        })
+        const data = resp
+        return data;
+    } catch (error) {
+        console.log(error)
+    }
 }
